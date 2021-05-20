fded6b6a93ac5fa6e7d74c117a0625b04812150a
AhmedShab <hello@ahmedshaaban.tech>
2021-05-20 14:12:30 +1200

    added lib

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
-interface Todo {
-  title: string;
-}
+import { Todo } from '@org2/data';
 
 @Component({
   selector: 'org2-root',
